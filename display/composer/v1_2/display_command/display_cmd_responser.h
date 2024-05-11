5af9cc70c65a2e703cf9b425835937384982a352
pxd2022 <pingxiandeng1@huawei.com>
2024-05-11 01:29:52 +0000

    update display/composer/v1_2/display_command/display_cmd_responser.h.
    Signed-off-by: pxd2022 <pingxiandeng1@huawei.com>

--- display/composer/v1_2/display_command/display_cmd_responser.h
+++ display/composer/v1_2/display_command/display_cmd_responser.h
@@ -29,6 +29,16 @@ namespace Composer {
 namespace V1_2 {
 using namespace OHOS::HDI::Base;
 
+typedef struct CommitInfo {
+    int32_t fence;
+    int32_t skipRet;
+    bool needFlush;
+    std::vector<uint32_t> compLayers;
+    std::vector<int32_t> compTypes;
+    std::vector<uint32_t> layers;
+    std::vector<int32_t> fences;
+} CommitInfo;
+
 template <typename Transfer, typename VdiImpl>
 class DisplayCmdResponser : public V1_1::DisplayCmdResponser<Transfer, VdiImpl> {
 public:
@@ -45,88 +55,53 @@ public:
 
     virtual ~DisplayCmdResponser() {}
 
-    int32_t ProcessRequestCmd(std::shared_ptr<CommandDataUnpacker> unpacker, int32_t cmd,
+    int32_t ProcessRequestCmd(std::shared_ptr<CommandDataUnpacker>& unpacker, int32_t cmd,
         const std::vector<HdifdInfo>& inFds, std::vector<HdifdInfo>& outFds)
     {
-        int32_t ret = HDF_SUCCESS;
-        std::vector<uint32_t> layers;
-        std::vector<int32_t> fences;
         HDF_LOGD("%{public}s: HDI 1.2 PackSection, cmd-[%{public}d] = %{public}s",
             __func__, cmd, CmdUtils::CommandToString(cmd));
-        switch (cmd) {
-            case REQUEST_CMD_COMMIT_AND_GET_RELEASE_FENCE:
-                OnCommitAndGetReleaseFence(unpacker, outFds, layers, fences);
-                break;
-            case REQUEST_CMD_PREPARE_DISPLAY_LAYERS:
-                OnPrepareDisplayLayers(unpacker);
-                break;
-            case REQUEST_CMD_SET_DISPLAY_CLIENT_BUFFER:
-                OnSetDisplayClientBuffer(unpacker, inFds);
-                break;
-            case REQUEST_CMD_SET_DISPLAY_CLIENT_DAMAGE:
-                OnSetDisplayClientDamage(unpacker);
-                break;
-            case REQUEST_CMD_COMMIT:
-                OnCommit(unpacker, outFds);
-                break;
-            case REQUEST_CMD_SET_LAYER_ALPHA:
-                OnSetLayerAlpha(unpacker);
-                break;
-            case REQUEST_CMD_SET_LAYER_REGION:
-                OnSetLayerRegion(unpacker);
-                break;
-            case REQUEST_CMD_SET_LAYER_CROP:
-                OnSetLayerCrop(unpacker);
-                break;
-            case REQUEST_CMD_SET_LAYER_ZORDER:
-                OnSetLayerZorder(unpacker);
-                break;
-            case REQUEST_CMD_SET_LAYER_PREMULTI:
-                OnSetLayerPreMulti(unpacker);
-                break;
-            case REQUEST_CMD_SET_LAYER_TRANSFORM_MODE:
-                OnSetLayerTransformMode(unpacker);
-                break;
-            case REQUEST_CMD_SET_LAYER_DIRTY_REGION:
-                OnSetLayerDirtyRegion(unpacker);
-                break;
-            case REQUEST_CMD_SET_LAYER_VISIBLE_REGION:
-                OnSetLayerVisibleRegion(unpacker);
-                break;
-            case REQUEST_CMD_SET_LAYER_BUFFER:
-                OnSetLayerBuffer(unpacker, inFds);
-                break;
-            case REQUEST_CMD_SET_LAYER_COMPOSITION_TYPE:
-                OnSetLayerCompositionType(unpacker);
-                break;
-            case REQUEST_CMD_SET_LAYER_BLEND_TYPE:
-                OnSetLayerBlendType(unpacker);
-                break;
-            case REQUEST_CMD_SET_LAYER_MASK_INFO:
-                OnSetLayerMaskInfo(unpacker);
-                break;
-            case CONTROL_CMD_REQUEST_END:
-                ret = OnRequestEnd(unpacker);
-                break;
-            case REQUEST_CMD_SET_LAYER_COLOR:
-                OnSetLayerColor(unpacker);
-                break;
-            default:
-                HDF_LOGE("%{public}s: not support this cmd, unpacked cmd = %{public}d", __func__, cmd);
-                ret = HDF_FAILURE;
-                break;
-        }
-        return ret;
+        if (cmd == REQUEST_CMD_COMMIT_AND_GET_RELEASE_FENCE) {
+            OnCommitAndGetReleaseFence(unpacker, outFds);
+        } else {
+            return V1_1::DisplayCmdResponser<Transfer, VdiImpl>::DisplayCmdResponser(unpacker, cmd, inFds, outFds);
+		    }
+        return HDF_SUCCESS;
     }
 
-    void ReplyCommitAndGetReleaseFence(std::vector<HdifdInfo>& outFds, uint32_t& devId, int32_t& fence,
-        int32_t& skipRet, std::vector<uint32_t>& compLayers, std::vector<int32_t>& compTypes, bool& needFlush,
-            std::vector<uint32_t>& layers, std::vector<int32_t>& fences)
+    void ReplyNotSkipInfo(uint32_t& devId, CommitInfo& commitInfo)
+    {
+		    DISPLAY_CHECK(replyPacker_->WriteUint32(devId) == false,
+            HDF_LOGE("%{public}s, write devId error", __func__));
+
+		    DISPLAY_CHECK(replyPacker_->WriteBool(commitInfo.needFlush) == false,
+            HDF_LOGE("%{public}s, write needFlush error", __func__));
+
+      // Write compLayers vector
+		    vectSize = static_cast<uint32_t>(commitInfo.compLayers.size());
+		    DISPLAY_CHECK(replyPacker_->WriteUint32(vectSize) == false,
+            HDF_LOGE("%{public}s, write compLayers.size error", __func__));
+
+		    for (uint32_t i = 0; i < vectSize; i++) {
+            DISPLAY_CHECK(replyPacker_->WriteUint32(commitInfo.compLayers[i]) == false,
+                HDF_LOGE("%{public}s, write compLayers error", __func__));
+		    }
+      // Write compTypes vector
+		    vectSize = static_cast<uint32_t>(commitInfo.compTypes.size());
+		    DISPLAY_CHECK(replyPacker_->WriteUint32(vectSize) == false,
+            HDF_LOGE("%{public}s, write compTypes.size error", __func__));
+
+		    for (uint32_t i = 0; i < vectSize; i++) {
+            DISPLAY_CHECK(replyPacker_->WriteUint32(commitInfo.compTypes[i]) == false,
+                HDF_LOGE("%{public}s, write compTypes error", __func__));
+		    }
+    }
+
+    void ReplyCommitAndGetReleaseFence(std::vector<HdifdInfo>& outFds, uint32_t& devId, CommitInfo& commitInfo)
     {
         int32_t ret = HDF_SUCCESS;
         uint32_t vectSize = 0;
 
-        HdifdParcelable fdParcel(fence);
+        HdifdParcelable fdParcel(commitInfo.fence);
         DISPLAY_CHK_CONDITION(ret, HDF_SUCCESS,
             CmdUtils::StartSection(REPLY_CMD_COMMIT_AND_GET_RELEASE_FENCE, replyPacker_),
             HDF_LOGE("%{public}s, StartSection error", __func__));
@@ -134,55 +109,31 @@ public:
         DISPLAY_CHK_CONDITION(ret, HDF_SUCCESS, CmdUtils::FileDescriptorPack(fdParcel.GetFd(), replyPacker_, outFds),
             HDF_LOGE("%{public}s, FileDescriptorPack error", __func__));
 
-        DISPLAY_CHECK(replyPacker_->WriteInt32(skipRet) == false,
+        DISPLAY_CHECK(replyPacker_->WriteInt32(commitInfo.skipRet) == false,
             HDF_LOGE("%{public}s, write skip validate return value error", __func__));
-        if (skipRet != HDF_SUCCESS) {
-            DISPLAY_CHECK(replyPacker_->WriteUint32(devId) == false,
-                HDF_LOGE("%{public}s, write devId error", __func__));
-
-            DISPLAY_CHECK(replyPacker_->WriteBool(needFlush) == false,
-                HDF_LOGE("%{public}s, write needFlush error", __func__));
-
-            // Write compLayers vector
-            vectSize = static_cast<uint32_t>(compLayers.size());
-            DISPLAY_CHECK(replyPacker_->WriteUint32(vectSize) == false,
-                HDF_LOGE("%{public}s, write compLayers.size error", __func__));
-
-            for (uint32_t i = 0; i < vectSize; i++) {
-                DISPLAY_CHECK(replyPacker_->WriteUint32(compLayers[i]) == false,
-                    HDF_LOGE("%{public}s, write compLayers error", __func__));
-            }
-            // Write compTypes vector
-            vectSize = static_cast<uint32_t>(compTypes.size());
-            DISPLAY_CHECK(replyPacker_->WriteUint32(vectSize) == false,
-                HDF_LOGE("%{public}s, write compTypes.size error", __func__));
-
-            for (uint32_t i = 0; i < vectSize; i++) {
-                DISPLAY_CHECK(replyPacker_->WriteUint32(compTypes[i]) == false,
-                    HDF_LOGE("%{public}s, write compTypes error", __func__));
-            }
+        if (commitInfo.skipRet != HDF_SUCCESS) {
+            ReplyNotSkipInfo(devId, commitInfo);
         }
 
             // Write layers vector
-            vectSize = static_cast<uint32_t>(layers.size());
-            DISPLAY_CHECK(replyPacker_->WriteUint32(vectSize) == false,
-                HDF_LOGE("%{public}s, write layers.size error", __func__));
+        vectSize = static_cast<uint32_t>(commitInfo.layers.size());
+        DISPLAY_CHECK(replyPacker_->WriteUint32(vectSize) == false,
+            HDF_LOGE("%{public}s, write layers.size error", __func__));
 
-            for (uint32_t i = 0; i < vectSize; i++) {
-                DISPLAY_CHECK(replyPacker_->WriteUint32(layers[i]) == false,
-                    HDF_LOGE("%{public}s, write layers error", __func__));
-            }
+        for (uint32_t i = 0; i < vectSize; i++) {
+            DISPLAY_CHECK(replyPacker_->WriteUint32(commitInfo.layers[i]) == false,
+                HDF_LOGE("%{public}s, write layers error", __func__));
+        }
 
             // Write fences vector
-            vectSize = static_cast<uint32_t>(fences.size());
-            DISPLAY_CHECK(replyPacker_->WriteUint32(vectSize) == false,
-                HDF_LOGE("%{public}s, write fences.size error", __func__));
+        vectSize = static_cast<uint32_t>(commitInfo.fences.size());
+        DISPLAY_CHECK(replyPacker_->WriteUint32(vectSize) == false,
+            HDF_LOGE("%{public}s, write fences.size error", __func__));
 
-            for (uint32_t i = 0; i < vectSize; i++) {
-                ret = CmdUtils::FileDescriptorPack(fences[i], replyPacker_, outFds);
-                DISPLAY_CHECK(ret != HDF_SUCCESS,
-                    HDF_LOGE("%{public}s, write fences error", __func__));
-            }
+        for (uint32_t i = 0; i < vectSize; i++) {
+            ret = CmdUtils::FileDescriptorPack(commitInfo.fences[i], replyPacker_, outFds);
+            DISPLAY_CHECK(ret != HDF_SUCCESS, HDF_LOGE("%{public}s, write fences error", __func__));
+        }
 
         DISPLAY_CHK_CONDITION(ret, HDF_SUCCESS, CmdUtils::EndSection(replyPacker_),
             HDF_LOGE("%{public}s, EndSection error", __func__));
@@ -198,19 +149,8 @@ public:
         }
     }
 
-    void OnCommitAndGetReleaseFence(std::shared_ptr<CommandDataUnpacker> unpacker,
-        std::vector<HdifdInfo>& outFds, std::vector<uint32_t>& layers, std::vector<int32_t>& fences)
+    void CommitInfoDump(void)
     {
-        DISPLAY_TRACE;
-
-        uint32_t devId = 0;
-        int32_t fence = -1;
-        bool isSupportSkipValidate = false;
-        int32_t ret = HDF_SUCCESS;
-        int32_t skipRet = HDF_FAILURE;
-        bool needFlush = false;
-        std::vector<uint32_t> compLayers;
-        std::vector<int32_t> compTypes;
 #ifdef DISPLAY_COMSPOER_DEBUG_DUMP
         const std::string SWITCH_ON = "on";
         const uint32_t DUMP_CACHE_SWITCH_LEN = 4;
@@ -220,7 +160,24 @@ public:
         if (SWITCH_ON.compare(dumpSwitch) == 0) {
             cacheMgr_->Dump();
         }
-#endif
+#endif 
+    }
+
+    void OnCommitAndGetReleaseFence(std::shared_ptr<CommandDataUnpacker>& unpacker,
+        std::vector<HdifdInfo>& outFds)
+    {
+        DISPLAY_TRACE;
+
+        uint32_t devId = 0;
+        
+        bool isSupportSkipValidate = false;
+        int32_t ret = HDF_SUCCESS;
+        CommitInfo commitInfo;
+        commitInfo.fence = -1;
+        commitInfo.skipRet = HDF_FAILURE;
+        commitInfo.needFlush = false;
+
+        CommitInfoDump();
         if (!unpacker->ReadUint32(devId)) {
             HDF_LOGE("%{public}s, read devId error", __func__);
             ret = HDF_FAILURE;
@@ -234,28 +191,28 @@ public:
 
         if (isSupportSkipValidate) {
             HdfTrace traceVdi("Commit", "HDI:DISP:HARDWARE");
-            skipRet = impl_->Commit(devId, fence);
+            commitInfo.skipRet = impl_->Commit(devId, commitInfo.fence);
         }
-        if (skipRet != HDF_SUCCESS) {
+        if (commitInfo.skipRet != HDF_SUCCESS) {
             {
                 HdfTrace traceVdi("PrepareDisplayLayers", "HDI:DISP:HARDWARE");
-                ret = impl_->PrepareDisplayLayers(devId, needFlush);
+                ret = impl_->PrepareDisplayLayers(devId, commitInfo.needFlush);
             }
             DISPLAY_CHECK(ret != HDF_SUCCESS, goto REPLY);
 
             {
                 HdfTrace traceVdi("GetDisplayCompChange", "HDI:DISP:HARDWARE");
-                ret = impl_->GetDisplayCompChange(devId, compLayers, compTypes);
+                ret = impl_->GetDisplayCompChange(devId, commitInfo.compLayers, commitInfo.compTypes);
             }
             DISPLAY_CHECK(ret != HDF_SUCCESS, goto REPLY);
         }
 
         HDF_LOGD("%{public}s, first commit with skipRet = %{public}d, fence = %{public}d, needFlush = %{public}d",
-            __func__, skipRet, fence, needFlush);
+            __func__, commitInfo.skipRet, commitInfo.fence, commitInfo.needFlush);
 		
         {
             HdfTrace traceVdi("GetDisplayReleaseFence", "HDI:DISP:HARDWARE");
-            ret = impl_->GetDisplayReleaseFence(devId, layers, fences);
+            ret = impl_->GetDisplayReleaseFence(devId, commitInfo.layers, commitInfo.fences);
         }
         
         if (ret != HDF_SUCCESS) {
@@ -263,7 +220,7 @@ public:
         }
 
 REPLY:
-        ReplyCommitAndGetReleaseFence(outFds, devId, fence, skipRet, compLayers, compTypes, needFlush, layers, fences);
+        ReplyCommitAndGetReleaseFence(outFds, devId, commitInfo);
         return;
     }
 
