fb0bcc5340b10d0f1b8861df733692d92ef30183
huangshengqing <huangshengqing8@huawei.com>
2024-05-31 07:18:09 +0000

    update camera/metadata/include/camera_device_ability_items.h.
    Signed-off-by: huangshengqing <huangshengqing8@huawei.com>

--- camera/metadata/include/camera_device_ability_items.h
+++ camera/metadata/include/camera_device_ability_items.h
@@ -1504,21 +1504,21 @@ typedef enum camera_device_metadata_tag {
      * camera device property, report current camera available profile level
      * (i.e. int32[] ...)
      */
-    OHOS_ABILITY_AVAILABLE_PROFILE_LEVEL
+    OHOS_ABILITY_AVAILABLE_PROFILE_LEVEL,
     /**
      * ohos.ability.availableConfigurations [static, int32[], public]
      *
      * camera device property, report current camera available configurations
      * (i.e. int32[] ...)
      */
-    OHOS_ABILITY_AVAILABLE_CONFIGURATIONS
+    OHOS_ABILITY_AVAILABLE_CONFIGURATIONS,
     /**
      * ohos.ability.conflictConfigurations [static, int32[], public]
      *
      * camera device property, report current camera conflict configurations
      * (i.e. int32[] ...)
      */
-    OHOS_ABILITY_CONFLICT_CONFIGURATIONS
+    OHOS_ABILITY_CONFLICT_CONFIGURATIONS,
     /**
      * ohos.camera.effectEnd
      *
