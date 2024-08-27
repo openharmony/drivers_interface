c574f8a3697b1b3013fb7dc51105a10ff6fd0445
l60050612 <liumin222@h-partners.com>
2024-08-27 05:44:20 +0000

    update display/composer/v1_0/display_command/display_cmd_responser.h.
    Signed-off-by: l60050612 <liumin222@h-partners.com>

--- display/composer/v1_0/display_command/display_cmd_responser.h
+++ display/composer/v1_0/display_command/display_cmd_responser.h
@@ -1104,8 +1104,8 @@ protected:
     /* fix fd leak */
     std::queue<BufferHandle *> delayFreeQueue_;
 private:
-    std::mutex request_mutex;
-    std::mutex reply_mutex;
+    std::mutex requestMutex_;
+    std::mutex replyMutex_;
 };
 using HdiDisplayCmdResponser = DisplayCmdResponser<SharedMemQueue<int32_t>, IDisplayComposerVdi>;
 } // namespace V1_0
