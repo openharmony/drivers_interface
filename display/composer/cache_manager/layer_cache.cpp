3a2a7b0bf325a966dbc5f2ad93c58e21b16521d1
l60050612 <liumin222@h-partners.com>
2024-09-29 14:38:31 +0800

    单框架CLD硬化圆角特性
    Signed-off-by: l60050612 <liumin222@h-partners.com>

--- display/composer/cache_manager/layer_cache.cpp
+++ display/composer/cache_manager/layer_cache.cpp
@@ -161,6 +161,10 @@ int32_t LayerCache::FreeMem(sptr<NativeBuffer>& buffer)
         HDF_LOGE("GetMapperService failed!");
         return HDF_FAILURE;
     }
+	int32_t ret = Unmap(buffer);
+	if( ret != HDF_SUCCESS) {
+        HDF_LOGE("Unmap failed!");		
+	}
     return mapperService->FreeMem(buffer);
 }
 
@@ -170,7 +174,16 @@ int32_t LayerCache::RegisterBuffer(sptr<NativeBuffer>& buffer)
     if (metaService == nullptr) {
         return HDF_FAILURE;
     }
-    return metaService->RegisterBuffer(buffer);
+    int32_t ret = metaService->RegisterBuffer(buffer);
+	if( ret != HDF_SUCCESS) {
+        HDF_LOGE("Register Buffer failed!");
+        return ret;
+	}
+    ret = Mmap(buffer);
+	if( ret != HDF_SUCCESS) {
+        HDF_LOGE("Mmap failed!");		
+	}
+    return HDF_SUCCESS;	
 }
 
 void LayerCache::Dump() const
