21b70632723516a2e9d639247cd9c5dd4fc1a068
cjj <chengjiajun7@h-partners.com>
2025-02-25 03:52:24 +0000

    update camera/metadata/include/camera_device_ability_items.h.
    Signed-off-by: cjj <chengjiajun7@h-partners.com>

--- camera/metadata/include/camera_device_ability_items.h
+++ camera/metadata/include/camera_device_ability_items.h
@@ -1450,7 +1450,7 @@ typedef enum camera_device_metadata_tag {
      * camera device property, set current camera support camera zoom performance
      * (i.e. byte ...)
      */
-     OHOS_ABILITY_CAMERA_ZOOM_PERFORMANCE,
+    OHOS_ABILITY_CAMERA_ZOOM_PERFORMANCE,
      /**
      * ohos.control.zoomBezierCurvePoint [static, byte, public]
       *
