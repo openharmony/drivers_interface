dc2bcfd3f974729c5f93e241344685f2cb338147
梁鹏辉 <liangpenghui2@huawei.com>
2025-07-28 15:15:52 +0800

    1
    Signed-off-by: 梁鹏辉 <liangpenghui2@huawei.com>

--- display/composer/cache_manager/cache_manager.h
+++ display/composer/cache_manager/cache_manager.h
@@ -181,7 +181,7 @@ public:
         uint32_t originalMaxCount = cacheCountMax_;
         if (count >= cacheCountMax_) {
             cacheCountMax_ = count;
-        } else if (Size() <= count || count == 0) {
+        } else if (Size() <= count) {
             cacheCountMax_ = count;
         } else {
             HDF_LOGE("%{public}s error: clientCacheCount can't be set, because cacheCountMax_ > count", __func__);
