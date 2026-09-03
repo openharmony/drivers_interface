12a5344efa92e6ca0a0b3805f139a6a2c5720dc3
zcr0917 <zhangchunrui1@h-partners.com>
2026-09-03 13:44:43 +0800

    动图一阶段防抖
    Signed-off-by: zcr0917 <zhangchunrui1@h-partners.com>

--- camera/metadata/include/camera_metadata_item_info.h
+++ camera/metadata/include/camera_metadata_item_info.h
@@ -358,6 +358,8 @@ static item_info_t g_ohosCameraControl[OHOS_DEVICE_CONTROL_END - OHOS_DEVICE_CON
        OHOS_DEVICE_CONTROL_START] = {"supportedGenerateOriginalImage", META_TYPE_BYTE, -1},
     [OHOS_CONTROL_GENERATE_ORIGINAL_IMAGE -
        OHOS_DEVICE_CONTROL_START] = {"generateOriginalImage", META_TYPE_BYTE, 1},
+    [OHOS_ABILITY_LIVEPHOTO_EIS_SUPPORTED -
+        OHOS_DEVICE_CONTROL_START] = {"opticalLivephotoFirstStageEisSupported", META_TYPE_INT32, -1},
 };
 
 static item_info_t g_ohosDeviceExposure[OHOS_DEVICE_EXPOSURE_END - OHOS_DEVICE_EXPOSURE_START] = {
